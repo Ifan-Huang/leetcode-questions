5704850e90d88ee741f73b8b12c20b414cd3f843
ifanhuang <ifanhuang@aliyun.com>
2022-08-29 09:52:40 +0800

    add day

---
+++ src/main/java/com/codeforc/algorithm/days/MinStartValue.java
@@ -0,0 +1,20 @@
+//package com.codeforc.algorithm.days;
+//
+///**
+// * 给你一个整数数组 nums。你可以选定任意的正数 startValue 作为初始值。
+// * 你需要从左到右遍历 nums数组，并将 startValue 依次累加上nums数组中的值。
+// * 请你在确保累加和始终大于等于 1 的前提下，选出一个最小的正数作为 startValue 。
+// */
+//public class MinStartValue {
+//
+//    public int minStartValue(int[] nums) {
+//        int sum = 0;
+//        int lastSum = sum;
+//        for (int num : nums) {
+//            sum +=num;
+//            if(lastSum > sum) {
+//
+//            }
+//        }
+//    }
+//}
