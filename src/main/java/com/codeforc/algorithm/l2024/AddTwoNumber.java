4d0b3a88f3527a8f8a2fc6376490ab374f5473ca
ifanhuang <ifanhuang@aliyun.com>
2025-01-24 14:21:16 +0800

    ansy

---
+++ src/main/java/com/codeforc/algorithm/l2024/AddTwoNumber.java
@@ -0,0 +1,11 @@
+package com.codeforc.algorithm.l2024;
+
+
+
+public class AddTwoNumber {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+
+}
---
+++ src/main/java/com/codeforc/algorithm/l2024/ListNode.java
@@ -0,0 +1,9 @@
+package com.codeforc.algorithm.l2024;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, com.codeforc.algorithm.l2024.ListNode next) { this.val = val; this.next = next; }
+}
